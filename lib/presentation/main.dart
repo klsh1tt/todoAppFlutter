953c75415ad69470851e9742ce42aab3a7c913fe
klsh1tt <klsh1tt@gmail.com>
2022-04-08 17:01:01 +0900

    floatingActionButtonの実装

--- lib/presentation/main.dart
+++ lib/presentation/main.dart
@@ -96,6 +96,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
